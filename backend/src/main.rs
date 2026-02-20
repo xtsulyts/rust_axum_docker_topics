000de0e6bf0c50c30fe5ad161727b5e7ced4552f
xtsulyts <531xtsulyts@gmail.com>
2026-02-19 21:37:48 -0300

    dependencias, actix, serde, tokio

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
 fn main() {
     println!("Hello, world!");
 }
+=======
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+>>>>>>> 501a1e3 (dependencias, actix, serde, tokio)
